b638439b5228fc01fbf31e30746ad94eccae6a2d
zmxn-PC\zmxn <kwor@163.com>
2017-06-28 15:24:49 +0800

    更新

--- reg.php
+++ reg.php
@@ -34,41 +34,89 @@
 		</script>
 	</head>
 	<body>
-		
-	<header class="mui-bar mui-bar-nav" style="padding-right: 15px;">
-			<h1 class="mui-title">注册</h1>
-			<button id='setting' class=" mui-pull-right mui-btn-link">开关</button>
-		</header>
-			<div class="mui-content">
-			<div class="mui-content-padded">
 	
-	<form class="mui-input-group">
-    <div class="mui-input-row">
-        <label>用户名</label>
-    <input type="text" class="mui-input-clear" placeholder="请输入用户名">
-    </div>
-    <div class="mui-input-row">
-        <label>密码</label>
-        <input type="password" class="mui-input-password" placeholder="请输入密码">
-    </div>
-    <div class="mui-input-row">
-        <label>密码</label>
-        <input type="repassword" class="mui-input-password" placeholder="请再次输入密码">
-    </div>
-    <div class="mui-button-row">
-        <button type="button" class="mui-btn mui-btn-primary" >确认</button>
-        <button type="button" class="mui-btn mui-btn-danger" >取消</button>
-    </div>
-</form>
-   
- 
-   
+
+	<body>
+		<header class="mui-bar mui-bar-nav">
+			<a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+			<h1 class="mui-title">注册</h1>
+		</header>
+		<div class="mui-content">
+			<form class="mui-input-group">
+				<div class="mui-input-row">
+					<label>账号</label>
+					<input id='account' type="text" class="mui-input-clear mui-input" placeholder="请输入账号">
+				</div>
+				<div class="mui-input-row">
+					<label>密码</label>
+					<input id='password' type="password" class="mui-input-clear mui-input" placeholder="请输入密码">
+				</div>
+				<div class="mui-input-row">
+					<label>确认</label>
+					<input id='password_confirm' type="password" class="mui-input-clear mui-input" placeholder="请确认密码">
+				</div>
+				<div class="mui-input-row">
+					<label>邮箱</label>
+					<input id='email' type="email" class="mui-input-clear mui-input" placeholder="请输入邮箱">
+				</div>
+			</form>
+			<div class="mui-content-padded">
+				<button id='reg' class="mui-btn mui-btn-block mui-btn-primary">注册</button>
+			</div>
+			<div class="mui-content-padded">
+				<p>注册真实可用，注册成功后的用户可用于登录，但是示例程序并未和服务端交互，用户相关数据仅存储于本地。</p>
 			</div>
 		</div>
-        <script src="js/mui.picker.min.js"></script>
 		<script src="js/mui.min.js"></script>
 		<script src="js/app.js"></script>
-	
-	 
+		<script>
+			(function($, doc) {
+				$.init();
+				$.plusReady(function() {
+					var settings = app.getSettings();
+					var regButton = doc.getElementById('reg');
+					var accountBox = doc.getElementById('account');
+					var passwordBox = doc.getElementById('password');
+					var passwordConfirmBox = doc.getElementById('password_confirm');
+					var emailBox = doc.getElementById('email');
+					regButton.addEventListener('tap', function(event) {
+						var regInfo = {
+							account: accountBox.value,
+							password: passwordBox.value,
+							email: emailBox.value
+						};
+						var passwordConfirm = passwordConfirmBox.value;
+						if (passwordConfirm != regInfo.password) {
+							plus.nativeUI.toast('密码两次输入不一致');
+							return;
+						}
+						app.reg(regInfo, function(err) {
+							if (err) {
+								plus.nativeUI.toast(err);
+								return;
+							}
+							plus.nativeUI.toast('注册成功');
+							/*
+							 * 注意：
+							 * 1、因本示例应用启动页就是登录页面，因此注册成功后，直接显示登录页即可；
+							 * 2、如果真实案例中，启动页不是登录页，则需修改，使用mui.openWindow打开真实的登录页面
+							 */
+							plus.webview.getLaunchWebview().show("pop-in",200,function () {
+								plus.webview.currentWebview().close("none");
+							});
+							//若启动页不是登录页，则需通过如下方式打开登录页
+//							$.openWindow({
+//								url: 'login.html',
+//								id: 'login',
+//								show: {
+//									aniShow: 'pop-in'
+//								}
+//							});
+						});
+					});
+				});
+			}(mui, document));
+		</script>
 	</body>
+
 </html>
