ba5dd25607f0e90bcb34e17bcaf2ffe2437f4aa1
zmxn-PC\zmxn <kwor@163.com>
2017-06-28 14:47:22 +0800

    提交

---
+++ reg.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no">
+		<title></title>
+		<link href="css/mui.min.css" rel="stylesheet" />
+	 <script src="js/mui.picker.min.js"></script>
+	 
+	<style>
+			ul {
+				font-size: 14px;
+				color: #8f8f94;
+			}
+			.mui-btn {
+				padding: 10px;
+			}
+		</style>
+		<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+		<script type="text/javascript">
+			//通过config接口注入权限验证配置
+			wx.config({
+			    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+			    appId: '', // 必填，公众号的唯一标识
+			    timestamp: '<?php echo time();?>', // 必填，生成签名的时间戳
+			    nonceStr: '<?php echo $nonceStr;?>', // 必填，生成签名的随机串
+			    signature: '<?php echo $signature;?>',// 必填，签名
+			    jsApiList: [] // 必填，需要使用的JS接口列表
+			});
+			//通过ready接口处理成功验证
+			wx.ready(function(){
+				// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后
+			});
+		</script>
+	</head>
+	<body>
+		
+	<header class="mui-bar mui-bar-nav" style="padding-right: 15px;">
+			<h1 class="mui-title">注册</h1>
+			<button id='setting' class=" mui-pull-right mui-btn-link">开关</button>
+		</header>
+			<div class="mui-content">
+			<div class="mui-content-padded">
+	
+	<form class="mui-input-group">
+    <div class="mui-input-row">
+        <label>用户名</label>
+    <input type="text" class="mui-input-clear" placeholder="请输入用户名">
+    </div>
+    <div class="mui-input-row">
+        <label>密码</label>
+        <input type="password" class="mui-input-password" placeholder="请输入密码">
+    </div>
+    <div class="mui-input-row">
+        <label>密码</label>
+        <input type="repassword" class="mui-input-password" placeholder="请再次输入密码">
+    </div>
+    <div class="mui-button-row">
+        <button type="button" class="mui-btn mui-btn-primary" >确认</button>
+        <button type="button" class="mui-btn mui-btn-danger" >取消</button>
+    </div>
+</form>
+   
+ 
+   
+			</div>
+		</div>
+        <script src="js/mui.picker.min.js"></script>
+		<script src="js/mui.min.js"></script>
+		<script src="js/app.js"></script>
+	
+	 
+	</body>
+</html>
